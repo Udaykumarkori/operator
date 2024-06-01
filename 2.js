6a2cce2132fe196b13211d5307ea6e488c838ff1
Udaykumarkori <162406048+Udaykumarkori@users.noreply.github.com>
2024-06-01 10:09:33 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,10 @@
+const input = require("readline-sync");
+
+let a = input.questionInt("number of date:-");
+let b =input.question("number of month:-");
+let c = input.questionInt("number of year:-");
+let d = a;
+let m = b;
+let y = c;
+console.log(d,m,y);
+
